5a68b86f3ff99dd3334cbe49ba3647e996be232f
ericktian <34035146+ericktian@users.noreply.github.com>
2018-01-15 15:37:09 -0500

    Add files via upload

---
+++ pinController.ino
@@ -0,0 +1,55 @@
+/*The circuit:
+RX is digital pin 10 (connect to TX of Xbee)
+TX is digital pin 11 (connect to RX of Xbee)
+Note:Not all pins on the Leonardo support change interrupts,
+so only the following can be used for RX:8, 9, 10, 11, 14 (MISO), 
+15 (SCK), 16 (MOSI).
+*/
+#include <SoftwareSerial.h>
+#include <Servo.h>
+//SoftwareSerial xbee(10, 11); // RX, TX
+const int rb = 5;
+const int lb = 7;
+
+int buttonState;
+int rbState;
+int lbState;
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  Serial1.begin(9600);
+    pinMode(rb, INPUT);
+    digitalWrite(rb, HIGH);
+    pinMode(lb, INPUT);
+    digitalWrite(lb, HIGH);
+    //internal pullup
+    }
+    void loop() { 
+      // run over and over
+      
+      rbState = digitalRead(rb);
+      lbState = digitalRead(lb);
+      
+      Serial.println(String(rbState) + " " + String(lbState));
+      // Serial.println(buttonState);
+      // Serial.println(String(bLFState) + " " + String(bLBState) 
+      // + " " + String(bRFState) + " " + String(bRBState));
+      delay(10);
+      if (rbState == 0) 
+      {
+        Serial1.write('r');
+      }
+      else if (lbState == 0) 
+      {
+        Serial1.write('l');
+        }
+      else {
+        Serial1.write('n');
+      }
+      
+      
+      }
+
+
+
---
+++ pinMach.ino
@@ -0,0 +1,69 @@
+/*The circuit:RX is digital pin 10 (connect to TX of Xbee)TX is digital pin 11 (connect to RX of Xbee)Note:Not all pins on the Leonardo support change interrupts,so only the following can be used for RX:8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).*/
+#include <SoftwareSerial.h>
+#include <Servo.h>
+SoftwareSerial xbee(10, 11); 
+// RX, TX
+
+const int button1 = A5;
+
+
+Servo myServoR;
+Servo myServoL;
+void setup() 
+{// Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  pinMode(13, OUTPUT);
+  myServoR.attach(12);
+  myServoL.attach(7);
+  digitalWrite(button1,HIGH);
+  }
+void leftFlip(){
+  // myServoL.write(15);
+  // delay(100);
+  myServoL.write(130); //165
+  delay(20);
+  myServoL.write(91);
+}
+void rightFlip(){
+  // myServoR.write(165);
+  // delay(100);
+  myServoR.write(20);
+  delay(20);
+  myServoR.write(91);
+  
+}
+void celebrate(){
+  myServoR.write(165);
+  myServoL.write(15);
+  delay(5000);
+  myServoR.write(91);
+  myServoL.write(91);
+}
+void loop() 
+{ // run over and over
+  myServoR.write(91);
+  myServoL.write(91);
+  while (Serial1.available() > 0) 
+  {
+
+    char c = Serial1.read();
+    // Serial.println(c);
+    
+    if (digitalRead(button1) == 0)
+    {
+      celebrate();
+    }
+    
+    if (c == 'r') {
+      rightFlip();
+    }
+    
+    else if (c == 'l') {
+      leftFlip();
+    }
+    
+    delay(10);
+ }
+  
+}
